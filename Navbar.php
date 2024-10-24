d64121ab765726e065cd73ab2a54e60babac30be
unknown <IT-User@SI00-ITUM403-02.CL1C.daa>
2024-10-24 13:27:04 +0200

    Added PHP's

---
+++ Navbar.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="index.php">home</a></li>
+        <li><a href="about.php">about</a></li>
+        <li><a href="contact.php">contact</a></li>
+        <li><a href="gallery.php">gallery</a></li>
+        <li><a href="user.php">user</a></li>
+    </ul>
+</nav>
